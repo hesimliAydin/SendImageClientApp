9c5aa5530fdc6447ee8629b11a29de7cf943b484
ZoreX <aydinhsimli@gmail.com>
2023-03-16 17:41:21 +0400

    done

--- SendImageAppClient/Views/MainWindow.xaml.cs
+++ SendImageAppClient/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SendImageAppClient.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace SendImageAppClient
         public MainWindow()
         {
             InitializeComponent();
+           DataContext= new MainWindowViewModel();
         }
     }
 }
